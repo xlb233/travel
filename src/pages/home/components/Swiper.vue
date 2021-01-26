723767d8335b7f0ed17b4dd3029762de233021ee
xlb233 <xyc121305596@gmail.com>
2021-01-26 16:04:50 +0800

    swiper completed

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl"/>
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 3000,
+        autoplayDisableOnInteraction: false
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://pic.90sjimg.com/design/00/70/29/90/574018952a48d.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://iwiz-blog-cms.c.yimg.jp/c/blog-cms/promotionalads/online/640_200.png'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
