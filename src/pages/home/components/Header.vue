f53554ce7afda080d7b1dd58b6f41a57dc6ff633
xlb233 <xyc121305596@gmail.com>
2021-01-26 14:47:49 +0800

    header completed

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left"><div class="iconfont back-icon">&#xe624;</div></div>
+    <div class="header-input"><span class="iconfont">&#xe632;</span> 输入城市/景点/游玩主题</div>
+    <div class="header-right">城市<span class="iconfont arrow-icon">&#xe64a;</span></div>
   </div>
 </template>
 
@@ -13,14 +13,18 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/variables.styl';
   .header
     display: flex
     line-height: .86rem
-    background: #00bce4
+    background: $bgColor
     color: #fff
     .header-left
       width: .64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-input
       flex: 1
       height: .64rem
@@ -29,9 +33,13 @@ export default {
       border-radius: .1rem
       margin-top: .12rem
       margin-left: .2rem
+      padding-left: .2rem
       color: #ccc
     .header-right
       width: 1.24rem
       float: right
       text-align: center
+      .arrow-icon
+        margin-left: -.24re
+        font-size: .24rem
 </style>
